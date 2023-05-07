27d8e84674a33bd899611afffb39ea248b084269
conwe007 <conwelljack97@gmail.com>
2023-05-07 15:23:11 -0400

    commit

--- main.js
+++ main.js
@@ -12,25 +12,25 @@ const DELAY = 100;
 
 let grid = new Grid();
 
-//grid.randomize(0.5);
-// grid.update();
-// grid.drawAll();
-grid.cells[0][0].setAlive();
-grid.cells[0][1].setAlive();
-grid.cells[1][1].setAlive();
-console.log(grid.numAliveNeighbors(0,0));
-console.log(grid.numAliveNeighbors(3,3));
+grid.randomize(0.5);
+grid.update();
 grid.drawAll();
+// grid.cells[0][0].setAlive();
+// grid.cells[0][1].setAlive();
+// grid.cells[1][1].setAlive();
+// console.log(grid.numAliveNeighbors(0,0));
+// console.log(grid.numAliveNeighbors(3,3));
+// grid.drawAll();
 
 function loop()
 {
     const start = Date.now();
 
-    // ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
-    // ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.fillRect(0, 0, width, height);
 
-    // grid = grid.update();
-    // grid.drawAll();
+    grid = grid.update();
+    grid.drawAll();
 
     while(Date.now() < start + DELAY);
 
